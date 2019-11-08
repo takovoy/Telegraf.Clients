bedb4fc22d5888e7a2c03bcd8b37b1159e210d8e
Aleksandr.Sustavov <bazaster1@gmail.com>
2019-11-08 10:09:03 +0300

    Hotfix

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,8 @@ import {Chanel} from "./models/telegraf-chanel.namespace";
 import {Statsd} from "./models/telegraf-statsd.namespace";
 import {Influx} from "./models/telegraf-influx.namespace";
 
-const Telegraf = {
+export const Telegraf = {
     UdpChannel: Chanel.UdpChannel,
     StatsdClient: Statsd.StatsdClient,
     InfluxClient: Influx.InfluxClient,
 };
-
-export default Telegraf;
